3fe6265064e17cf2e11f7dc392e203b7dd6b2955
Saba <69626492+smchedlidze826@users.noreply.github.com>
2023-03-17 01:11:45 +0400

    Finished UI

---
+++ src/app/core/intefaces/games.ts
@@ -0,0 +1,12 @@
+export interface Games {
+    game_id: string,
+    name: string,
+    provider: string,
+    providerName: string,
+    image: string,
+    url: string,
+    order: number,
+    gameId: string,
+    image2: string
+    tags: string[],
+}
